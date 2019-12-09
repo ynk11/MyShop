6b53565a809ced47264ddac90a4489bc8a6eeef8
yaboites <yaboites@hotmail.com>
2019-12-09 20:20:49 +0100

    Initial Solution Projects

---
+++ MyShop/MyShop.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.Web.Startup))]
+namespace MyShop.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
