c48ab5707e6682ddda84fc25dd2b248684b5951d
yaboites <yaboites@hotmail.com>
2019-12-29 16:38:51 +0100

    SQLRepository

--- MyShop/MyShop.Web/App_Start/UnityConfig.cs
+++ MyShop/MyShop.Web/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using MyShop.Core.Contracts;
 using MyShop.Core.Models;
 using MyShop.DataInMemory;
+using MyShop.DataSQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace MyShop.Web
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
